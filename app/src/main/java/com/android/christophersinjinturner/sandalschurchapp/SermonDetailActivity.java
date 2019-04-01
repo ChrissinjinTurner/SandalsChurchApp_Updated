3107340e300c92883bae684dfc0792f258673c8d
Chris Turner <Chrissinjinturner@gmail.com>
2019-04-01 16:53:02 -0700

    hid nav bar on fullscreen video

--- app/src/main/java/com/android/christophersinjinturner/sandalschurchapp/SermonDetailActivity.java
+++ app/src/main/java/com/android/christophersinjinturner/sandalschurchapp/SermonDetailActivity.java
@@ -127,6 +127,20 @@ public class SermonDetailActivity extends AppCompatActivity {
      */
     private void initFullscreenDialog() {
         mFullScreenDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen) {
+            @Override
+            public void onWindowFocusChanged(boolean hasFocus) {
+                super.onWindowFocusChanged(hasFocus);
+                View decorView = getWindow().getDecorView();
+                if (hasFocus) {
+                    decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+            }
+
             public void onBackPressed() {
                 if (mExoPlayerFullscreen)
                     closeFullscreenDialog();
